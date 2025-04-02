3ca498ac1baeae99974d700b9cd04aa7c49739e3
pedsiem <pedro.cerdano-vicente@siemens.com>
2025-04-02 15:10:55 +0200

    added preview option for fzf preview view

--- .svic321/000_inicio.zsh
+++ .svic321/000_inicio.zsh
@@ -55,6 +55,26 @@ fzf_history_insert() {
   fi
 }
 
+# FZF setup for preview
+
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo $'{}"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview "bat -n --color=always --line-range :500 {}" "$@" ;;
+  esac
+}
+
 # Bind the widget to Ctrl-O
 zle -N fzf_history_insert
 bindkey '^O' fzf_history_insert
