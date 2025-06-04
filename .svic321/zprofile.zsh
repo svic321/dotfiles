9a11cbfc2850030c7072e3d4548cc0c8515e48c4
Pedro Cerdano Vicente <pedro.cerdano-vicente@siemens.com>
2025-06-04 12:02:50 +0200

    removed ssh-agent and use ohmyzsh preconfigured ssh-agent plugin instead

--- .svic321/zprofile.zsh
+++ .svic321/zprofile.zsh
@@ -10,43 +10,20 @@ if [[ -d ~/.local/bin ]]; then
 fi
 
 # SSH_ENV="$HOME/.ssh/agent-environment"
-
-# function start_agent {
-#     echo "Initialising new SSH agent..."
-#     /usr/bin/ssh-agent | sed 's/^echo/#echo/' >"$SSH_ENV"
-#     echo succeeded
-#     chmod 600 "$SSH_ENV"
-#     . "$SSH_ENV" >/dev/null
-#     /usr/bin/ssh-add; 
+#
+# start_ssh_agent() {
+#   echo "Starting new ssh-agent..."
+#   ssh-agent -s | sed 's/^echo/#echo/' > "$SSH_ENV"
+#   chmod 600 "$SSH_ENV"
+#   source "$SSH_ENV"
 # }
 #
-# # Source SSH settings, if applicable
-#
 # if [ -f "$SSH_ENV" ]; then
-#     . "$SSH_ENV" >/dev/null
-#     #ps $SSH_AGENT_PID doesn't work under Cygwin
-#     ps -ef | grep $SSH_AGENT_PID | grep ssh-agent$ >/dev/null || {
-#         start_agent
-#     } 
+#   source "$SSH_ENV" > /dev/null
+#   if ! kill -0 "$SSH_AGENT_PID" 2>/dev/null; then
+#     start_ssh_agent
+#   fi
 # else
-#     start_agent 
+#   start_ssh_agent
 # fi
 #
-SSH_ENV="$HOME/.ssh/agent-environment"
-
-start_ssh_agent() {
-  echo "Starting new ssh-agent..."
-  ssh-agent -s | sed 's/^echo/#echo/' > "$SSH_ENV"
-  chmod 600 "$SSH_ENV"
-  source "$SSH_ENV"
-}
-
-if [ -f "$SSH_ENV" ]; then
-  source "$SSH_ENV" > /dev/null
-  if ! kill -0 "$SSH_AGENT_PID" 2>/dev/null; then
-    start_ssh_agent
-  fi
-else
-  start_ssh_agent
-fi
-
