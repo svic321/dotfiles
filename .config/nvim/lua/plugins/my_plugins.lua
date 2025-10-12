a952481d83176df56cbb58e87741933a09b4df5d
pedro <pedrolp321@outlook.com>
2025-10-12 03:14:17 +0200

    improvements in look and functionality

--- .config/nvim/lua/plugins/my_plugins.lua
+++ .config/nvim/lua/plugins/my_plugins.lua
@@ -6,11 +6,15 @@ return {
       on_colors = function(colors)
         colors.border = colors.blue1
       end,
+      on_highlights = function(highlights, colors)
+        highlights.BufferlineindicatorVisible = { bg = "#636DA6" }
+        highlights.BufferlineindicatorSelected = { bg = colors.blue1 }
+      end,
     },
   },
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
@@ -18,4 +22,20 @@ return {
     opts = {},
   },
   { "bullets-vim/bullets.vim" },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.inactive_winbar = {
+        lualine_a = { "filename" },
+        lualine_b = { "filetype" },
+        lualine_z = { "location" },
+      }
+    end,
+  },
 }
